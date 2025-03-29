8f05c0534633c16790bcc432f0629bc3b10c8216
Hiren Fadadu <hirenfaldu9697@gmail.com>
2025-03-29 16:50:47 +0530

    Moved helper methods in separate file

--- ECommerceScreens/ECommerceScreensTests/WalkthroughVCTests.swift
+++ ECommerceScreens/ECommerceScreensTests/WalkthroughVCTests.swift
@@ -23,52 +23,6 @@ final class WalkthroughVCTests: XCTestCase {
         assert(sut: sut, isRendering: items)
     }
     
-    private func assert(sut: WalkthroughVC,
-                        isRendering items: [WalkthroughItem],
-                        file: StaticString = #file,
-                        line: UInt = #line) {
-        XCTAssertEqual(sut.numberOfRenderedItems,
-                       items.count,
-                       "Expected walkthrough to render \(items.count) items, But it rendered \(sut.numberOfRenderedItems) items",
-                       file: file,
-                       line: line)
-        
-        for (index, item) in items.enumerated() {
-            assert(sut: sut,
-                   configured: item,
-                   at: index,
-                   file: file,
-                   line: line)
-        }
-    }
-    
-    private func assert(sut: WalkthroughVC,
-                        configured item: WalkthroughItem,
-                        at index: Int,
-                        file: StaticString = #filePath,
-                        line: UInt = #line) {
-        guard let view = sut.simulateWalkthroughItemVisible(at: index) else {
-            return XCTFail("Expected non-nil view at: \(index), but got nil instead",
-                           file: file,
-                           line: line)
-        }
-        XCTAssertEqual(view.imageView.image,
-                       item.image,
-                       "Expected view at: \(index) to have image: \(item.image), but got \(String(describing: view.imageView.image)) instead",
-                       file: file,
-                       line: line)
-        XCTAssertEqual(view.titleLabel.text,
-                       item.title,
-                       "Expected view at: \(index) to have title text: '\(item.title)', but got '\(String(describing: view.titleLabel.text))' instead",
-                       file: file,
-                       line: line)
-        XCTAssertEqual(view.subtitleLabel.text,
-                       item.subtitle,
-                       "Expected view at: \(index), to have subtitle text: '\(item.subtitle)', but got '\(String(describing: view.subtitleLabel.text))' instead",
-                       file: file,
-                       line: line)
-    }
-    
     // MARK: - Helper
     private func getDummyItems() -> [WalkthroughItem] {
         [
@@ -89,7 +43,6 @@ final class WalkthroughVCTests: XCTestCase {
                          subtitle: "Fifth subtitle"),
         ]
     }
-
 }
 
 
