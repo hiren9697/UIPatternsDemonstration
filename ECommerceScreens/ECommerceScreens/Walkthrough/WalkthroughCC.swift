c6d7be31a04d4c36cbbc8c71e95d9a8dc8ec2021
Hiren Fadadu <hirenfaldu9697@gmail.com>
2025-04-04 08:13:47 +0530

    Layout UI components in `WalkthroughCC`

--- ECommerceScreens/ECommerceScreens/Walkthrough/WalkthroughCC.swift
+++ ECommerceScreens/ECommerceScreens/Walkthrough/WalkthroughCC.swift
@@ -11,4 +11,43 @@ public class WalkthroughCC: UICollectionViewCell {
     let imageView: UIImageView = UIImageView()
     let titleLabel: UILabel = UILabel()
     let subtitleLabel: UILabel = UILabel()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("Init called")
+        layoutUIComponents()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("This class is designed programmatically, so shouldn't be initialized via storyboard")
+    }
+    
+    // MARK: - UI Helpers
+    private func layoutUIComponents() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        titleLabel.textColor = .black
+        titleLabel.textAlignment = .center
+        
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.font = .systemFont(ofSize: 14)
+        subtitleLabel.textColor = .gray
+        subtitleLabel.numberOfLines = 0
+        subtitleLabel.textAlignment = .center
+
+        let stackView = UIStackView(arrangedSubviews: [imageView, titleLabel, subtitleLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        contentView.addSubview(stackView)
+        stackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15).isActive = true
+    }
 }
