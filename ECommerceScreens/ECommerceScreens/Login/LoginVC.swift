552da8dae29fe60f1c833151cd81931767a591db
Hiren Fadadu <hirenfaldu9697@gmail.com>
2025-04-06 16:47:32 +0530

    Renamed walkthrough diagrams

---
+++ ECommerceScreens/ECommerceScreens/Login/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  ECommerceScreens
+//
+//  Created by Hirenkumar Fadadu on 06/04/25.
+//
+
+import UIKit
+
+public class LoginVC: UIViewController {
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("LoginVC is designed programmatically, it shouldn't be initialized from storyboard")
+    }
+}
---
+++ ECommerceScreens/ECommerceScreensTests/Login/LoginVCTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginVCTests.swift
+//  ECommerceScreensTests
+//
+//  Created by Hirenkumar Fadadu on 06/04/25.
+//
+
+import XCTest
+import ECommerceScreens
+
+final class LoginVCTests: XCTestCase {
+
+    func test_welcomeText_isConfigured() {
+        let sut = LoginVC()
+    }
+
+}
