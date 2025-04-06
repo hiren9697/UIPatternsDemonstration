efaec57d6b60a5d203f4bb776c491cd5f368d447
Hiren Fadadu <hirenfaldu9697@gmail.com>
2025-04-06 17:06:20 +0530

    Merged tests for correct text and attributes in single test

--- ECommerceScreens/ECommerceScreensTests/Login/LoginVCTests.swift
+++ ECommerceScreens/ECommerceScreensTests/Login/LoginVCTests.swift
@@ -9,25 +9,20 @@ import XCTest
 @testable import ECommerceScreens
 
 final class LoginVCTests: XCTestCase {
-    func test_welcomeText_isCorrect() {
+    func test_welcomeText_isConfiguredWithCorrectTextAndAttributes() {
         // Arrange & Act
         let sut = makeSUT()
         
         // Assert
-        XCTAssertEqual(sut.welcomeLabel.text, "Welcome\nBack!")
-    }
-    
-    func test_welcomeText_hasCorrectAttributes() {
-        // Arrange & Act
-        let sut = makeSUT()
-        
-        // Assert
+        XCTAssertEqual(sut.welcomeLabel.text,
+                       "Welcome\nBack!",
+                       "Expected text to be Welcome\nBack!, but got \(String(describing: sut.welcomeLabel.text)) instead")
         XCTAssertEqual(sut.welcomeLabel.font,
                        .boldSystemFont(ofSize: 36),
-                       "Expected font to be boldSystemFont(ofSize: 36)")
+                       "Expected font to be boldSystemFont(ofSize: 36), but got \(String(describing: sut.welcomeLabel.font)) instead")
         XCTAssertEqual(sut.welcomeLabel.textColor,
                        AppColors.c000000,
-                       "Expected textColor to be AppColors.c000000")
+                       "Expected textColor to be AppColors.c000000, but got \(String(describing: sut.welcomeLabel.textColor)) instead")
     }
     
     // MARK: - Helper
