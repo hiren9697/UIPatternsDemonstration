6c96570deac36b661381ca07ec5139de73ca71d3
Hiren Fadadu <hirenfaldu9697@gmail.com>
2025-04-23 08:24:30 +0530

    Implemented `PRSortOption`

---
+++ ECommerceScreens/ECommerceScreens/Products/PRSortOption.swift
@@ -0,0 +1,20 @@
+//
+//  PRSortOption.swift
+//  ECommerceScreens
+//
+//  Created by Hirenkumar Fadadu on 23/04/25.
+//
+
+import Foundation
+
+public enum PRSortOption {
+    case lowToHigh
+    case highToLow
+    
+    var title: String {
+        switch self {
+        case .lowToHigh: "Low to High"
+        case .highToLow: "High to Low"
+        }
+    }
+}
