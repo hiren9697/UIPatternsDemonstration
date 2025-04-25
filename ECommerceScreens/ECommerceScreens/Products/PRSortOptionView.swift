d6362051721f9d58a1a565073b3de296573d9469
Hiren Fadadu <hirenfaldu9697@gmail.com>
2025-04-25 08:07:32 +0530

    Setup constraints

--- ECommerceScreens/ECommerceScreens/Products/PRSortOptionView.swift
+++ ECommerceScreens/ECommerceScreens/Products/PRSortOptionView.swift
@@ -13,8 +13,8 @@ public enum PRSortOptionState {
     
     public var image: UIImage {
         switch self {
-        case .selected: return UIImage(systemName: "circle")!
-        case .unSelected: return UIImage(systemName: "circle.circle")!
+        case .selected: return UIImage(systemName: "circle.circle")!
+        case .unSelected: return UIImage(systemName: "circle")!
         }
     }
 }
@@ -36,6 +36,7 @@ public class PRSortOptionView: UIView {
         self.completion = completion
         super.init(frame: .zero)
         configureInitialUI()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -44,12 +45,43 @@ public class PRSortOptionView: UIView {
     
     private func configureInitialUI() {
         titleLabel.text = type.title
+        selectionImageView.tintColor = .black
+        selectionImageView.contentMode = .scaleAspectFit
         selectionImageView.image = isSelected ? PRSortOptionState.selected.image : PRSortOptionState.unSelected.image
         button.addTarget(self, action: #selector(handleButtonTap), for: .touchUpInside)
     }
     
     @objc func handleButtonTap() {
         completion(type)
+    }
+    
+    private func setupConstraints() {
+        titleLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 250), for: .horizontal)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        selectionImageView.setContentHuggingPriority(UILayoutPriority(rawValue: 251), for: .horizontal)
+        selectionImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let stackView = UIStackView(arrangedSubviews: [selectionImageView, titleLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        
+        let containerView = UIView()
+        self.addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        containerView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+
+        containerView.addSubview(stackView)
+        stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 20).isActive = true
+        stackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 15).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -15).isActive = true
     }
     
     // Test properties
