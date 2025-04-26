032a4223d88aa1dc029c3818b2837d75bd0b8ff1
Hiren Fadadu <hirenfaldu9697@gmail.com>
2025-04-26 09:12:21 +0530

    Made changes in `makeSUT`

--- ECommerceScreens/ECommerceScreensTests/Products/BasicMVCTests/SortViewTests/PRSortViewTests.swift
+++ ECommerceScreens/ECommerceScreensTests/Products/BasicMVCTests/SortViewTests/PRSortViewTests.swift
@@ -11,15 +11,18 @@ import ECommerceScreens
 class PRSortViewTests: XCTestCase {
     func test_present_addsSortViewInParentView() {
         // Arrange & Act
-        let (sut, viewController) = makeSUT()
+        let parentView = UIView()
+        let sut = makeSUT(parentView: parentView)
 
         // Assert
-        XCTAssertEqual(sut.superview, viewController.view)
+        XCTAssertEqual(sut.superview, parentView)
     }
     
     func test_clickOnBackView_dismisses() {
         // Arrange
-        let (sut, _) = makeSUT()
+        let parentView = UIView()
+        let sut = makeSUT(parentView: parentView)
+        XCTAssertNotNil(sut.superview)
         
         // Act
         sut.simulateBackgroundTap()
@@ -30,7 +33,7 @@ class PRSortViewTests: XCTestCase {
     
     func test_selectedOption_isNotConfigured_whenGivenNilValue() {
         // Arrange & Act
-        let (sut, _) = makeSUT()
+        let sut = makeSUT()
         
         // Assert
         XCTAssertNil(sut.selectedOption)
@@ -39,7 +42,7 @@ class PRSortViewTests: XCTestCase {
     func test_selectedOption_isConfigured_whenGivenNonNilValue() {
         // Arrange & Act
         let option: PRSortOption = PRSortOption.highToLow
-        let (sut, _) = makeSUT(selectedOption: option)
+        let sut = makeSUT(selectedOption: option)
         
         // Assert
         XCTAssertEqual(sut.selectedOption, option)
@@ -48,7 +51,7 @@ class PRSortViewTests: XCTestCase {
     func test_clickOnBackView_doesNotCallCompletion() {
         // Arrange
         var completionCalls: [PRSortOption?] = []
-        let (sut, _) = makeSUT(completion: { selectedSortOption in completionCalls.append(selectedSortOption) })
+        let sut = makeSUT(completion: { selectedSortOption in completionCalls.append(selectedSortOption) })
         
         // Act
         sut.simulateBackgroundTap()
@@ -60,7 +63,7 @@ class PRSortViewTests: XCTestCase {
     func test_choosingHighToLowPrice_completesWithHighToLow() {
         // Arrange
         var completions: [PRSortOption?] = []
-        let (sut, _) = makeSUT(completion: { completions.append($0) })
+        let sut = makeSUT(completion: { completions.append($0) })
         
         // Act
         sut.simulateHighToLowPriceTap()
@@ -72,7 +75,7 @@ class PRSortViewTests: XCTestCase {
     func test_choosingLowToHighPrice_completesWithLowToHigh() {
         // Arrange
         var completions: [PRSortOption?] = []
-        let (sut, _) = makeSUT(completion: { completions.append($0) })
+        let sut = makeSUT(completion: { completions.append($0) })
         
         // Act
         sut.simulateLowToHighPriceTap()
@@ -81,17 +84,29 @@ class PRSortViewTests: XCTestCase {
         XCTAssertEqual(completions, [PRSortOption.lowToHigh])
     }
     
+//    func test_choosingLowToHighPrice_dismisses() {
+//        // Arrange
+//        let (sut, viewController) = makeSUT(completion: { _ in })
+//        XCTAssertNotNil(sut.superview)
+//        
+//        // Act
+//        sut.simulateLowToHighPriceTap()
+//        
+//        // Assert
+//        XCTAssertNil(sut.superview)
+//    }
+    
     // MARK: - Helper
-    private func makeSUT(selectedOption: PRSortOption? = nil,
+    private func makeSUT(parentView: UIView = UIView(),
+                         selectedOption: PRSortOption? = nil,
                          completion: @escaping PRSortView.Completion = { _ in },
                          file: StaticString = #filePath,
-                         line: UInt = #line) -> (PRSortView, UIViewController) {
-        let viewController = UIViewController()
-        let sortView = PRSortView.present(in: viewController.view,
+                         line: UInt = #line) -> PRSortView {
+        let sortView = PRSortView.present(in: parentView,
                                           withSelectedOption: selectedOption,
                                           completion: completion)
-        trackMemory(for: viewController, file: file, line: line)
+        trackMemory(for: parentView, file: file, line: line)
         trackMemory(for: sortView, file: file, line: line)
-        return (sortView, viewController)
+        return sortView
     }
 }
