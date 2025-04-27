c29897c2176da7b0b428ef3b743cfa7f586b6853
Hiren Fadadu <hirenfaldu9697@gmail.com>
2025-04-27 13:51:16 +0530

    Created `Product`

---
+++ ECommerceScreens/ECommerceScreens/Products/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  ECommerceScreens
+//
+//  Created by Hirenkumar Fadadu on 27/04/25.
+//
+
+import Foundation
+
+struct Product {
+    let name: String
+    let description: String
+    let price: Double
+    let imagePath: String
+}
